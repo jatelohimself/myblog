3e35f75d886a04086fb07461ec0159bdc20f57c6
Dennis ochieng <d.dochieng@gmail.com>
2017-06-29 13:46:56 +0300

    Comments done

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -12,15 +12,28 @@
     <input type="submit" value="delete" class="btn-danger">
 </form>
 <hr>
+<h3>Comments</h3>
+<?php if($comments): ?>
+    <?php foreach ($comments as $comment):?>
+        <div class="well">
+             <h5><?php echo $comment['body'];?> [by <strong><?php echo $comment['name']?></strong>]</h5>
+        </div>
+    <?php endforeach;?>
+    <?php else:?>
+    <p>No comments to display</p>
+    <?php endif?>
+
+<hr>
 <h3>Add Comments</h3>
+<?php echo validation_errors()?>
 <?php echo form_open('comments/create/'.$post['id']);?>
     <div class="form-group">
         <label>Name</label>
-        <input type="text" name="email" class="form-control">
-        <label>Email</label>
         <input type="text" name="name" class="form-control">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control">
         <label>Body</label>
-        <textarea name="name" class="form-control"></textarea>
+        <textarea name="body" class="form-control"></textarea>
     </div>
     <input type="hidden" name="slug" value="<?php echo $post['slug'];?>">
     <button type="submit" class="btn btn-default">Submit</button>
